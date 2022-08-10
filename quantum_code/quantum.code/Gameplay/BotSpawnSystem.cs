6ddc67dd9afc324c2e39133c2d403f001e6d7e2e
Igor Semenov <igor.semenov.minsk@gmail.com>
2022-08-10 21:37:14 +0300

    Bot spawning system

---
+++ quantum_code/quantum.code/Gameplay/BotSpawnSystem.cs
@@ -0,0 +1,19 @@
+﻿namespace Quantum.Gameplay;
+
+public class BotSpawnSystem : SystemSignalsOnly
+{
+    private const string BOT_PROTOTYPE_PATH = 
+        "Resources/DB/Prefabs/EnemyBot|EntityPrototype";
+    
+    public override unsafe void OnInit(Frame f)
+    {
+        var botPrototype = f.FindAsset<EntityPrototype>(BOT_PROTOTYPE_PATH);
+        var botEntity = f.Create(botPrototype);
+        
+        var filter = f.Filter<SpawnPoint, Transform3D>();
+        filter.Next(out _, out _, out var spawnTransform);
+        
+        var botTransform = f.Unsafe.GetPointer<Transform3D>(botEntity);
+        botTransform->Position = spawnTransform.Position;
+    }
+}
